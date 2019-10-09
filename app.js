da54b9b4e4eb7c94e6752ccd3a5b06b6a4196755
Irina <yutatranslation@gmail.com>
2019-10-10 00:50:11 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,6 @@
+let password = prompt('Enter your password', '');
+if (password == 1234) {
+	alert ('Valid password');
+} else {
+	alert ('Invalid password');
+}
---
+++ app1.js
@@ -0,0 +1,10 @@
+let number = prompt ('Угадайте число','');
+if (number <5) {
+	alert('Недолет');
+} 
+if (number >5) {
+	alert('Перелет');
+} 
+if (number == 5) {
+	alert('Красавчик');
+}
